7fbe8eb00102ac8756e17826eed2f850449c8b28
michoug <gregoire.michoud@kaust.edu.sa>
2023-09-15 16:09:09 +0200

    added some code and fixed packages

---
+++ code/AbundanceEukaryotesMAGs.r
@@ -0,0 +1,76 @@
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## Author: Dr. Grégoire Michoud
+##
+## Date Created: 2023-09-15
+##
+## Copyright (c) Grégoire Michoud, 2023
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+library("RColorBrewer")
+library("ggplot2")
+library(tidyverse)
+
+source("customFunctions/plot_functions.R")
+
+datTax	<- read_tsv("../Eukaryotes/Phylofisher/Eukaryotic_MAGs_taxonomy.txt", col_names = T)
+
+datCov	<- read_tsv("../Eukaryotes/Eukaryotes_MAGs_cov_norm.txt")
+map	<- read_tsv("../metadata_NOMIS_sed_chem.txt")
+busco <- read_tsv("../Eukaryotes/busco_30.txt")
+
+map_sel <- map %>%
+  select(Sample, Site_c)
+
+datToPlot	<- datCov %>%
+  pivot_longer(cols = !MAGs)%>%
+  right_join(map_sel, by = join_by(name == Sample))%>%
+  left_join(datTax, by = join_by(MAGs)) %>%
+  filter(MAGs %in% busco$MAGs)%>%
+  group_by(Lower_Taxonomy, Site_c) %>%
+  summarise(sum	= sum(value)) %>%
+  ungroup()%>%
+  group_by(Site_c)%>%
+  mutate(percentage	= sum / sum(sum))%>%
+  mutate(Site_c = if_else(Site_c == "New_Zealand", "New Zealand", Site_c))%>%
+  mutate(Lower_Taxonomy = gsub("_", " - ", Lower_Taxonomy))
+
+datToPlot$Tax	<- as.factor(datToPlot$Lower_Taxonomy)
+datToPlot$Tax	<-
+  factor(datToPlot$Tax, levels = rev(levels(datToPlot$Tax)))
+datToPlot$Tax	<-
+  factor(datToPlot$Tax, levels = rev(levels(datToPlot$Tax)))
+
+p1	<-
+  ggplot(datToPlot, aes(x = Site_c, y = percentage*100, fill = Tax)) +
+  geom_bar(stat	= "identity") +
+  scale_fill_manual(name = "Most Abundant Taxa", values = colorRampPalette(brewer.pal(10, "Paired"))(length(unique(datToPlot$Tax)))) +
+  theme_classic()+
+  labs(y = "Percentage of Coverage (%)", x = NULL)+
+  theme(
+    panel.grid.major	= element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background	= element_blank(),
+    legend.background = element_blank(),
+    text	= element_text(colour = "black",size = 12),
+    axis.text = element_text(color = "black", size = 12),
+    axis.title = element_text(color = "black", size = 14),
+    axis.line	= element_line(colour = "black")
+  ) +
+  theme(strip.background	= element_rect(fill = "transparent")) +
+  theme(strip.text	= element_text(colour = 'black', size = 12))
+
+p1
+ggsave_fitmax("Figures/barplotNOMIS_Eukaryotic_MAGS_Region_above30.pdf", p1, maxwidth = 13)
+
+
---
+++ code/Genomad_Sankey_iPhop.r
@@ -0,0 +1,151 @@
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## Author: Dr. Grégoire Michoud
+##
+## Date Created: 2023-09-15
+##
+## Copyright (c) Grégoire Michoud, 2023
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
+library("ggplot2")
+library(tidyverse)
+library(ggsankey)
+
+source("customFunctions/plot_functions.R")
+
+dat_euk <- read_tsv("../Virus/Genomad_Virus_Eukaryotes/Genomad_Virus_Eukaryotes.txt")
+dat_prok <- read_tsv("../VirusGenomad_Virus_host/NOMIS_genomad_virus_summary.txt")
+dat_euk_vtax <- read_tsv("../VirusGenomad_Virus_Eukaryotes/allEukviruses.names.out", col_names = F)
+dat_prok_vtax <- read_tsv("../VirusGenomad_Virus_host/allhostviruses.names.out", col_names = F)
+dat_euk_ptax <- read_tsv("../Eukaryotes/Phylofisher/Above30perc/metadata_input_above30.tsv")
+dat_prok_ptax <- read_tsv("../Prokaryotes/TaxQual/NOMIS_MAGS_tax.tsv")
+
+colnames(dat_euk_vtax) <- c("class", "seq_name", "taxId", "taxonomy")
+colnames(dat_prok_vtax) <- c("class", "seq_name", "taxId", "taxonomy")
+
+dat_euk_ptax$MAGs <- gsub("-","_", dat_euk_ptax$MAGs)
+
+dat_virus_euk <- dat_euk%>%
+  left_join(dat_euk_vtax,by = c("seq_name"),relationship = "many-to-many")%>%
+  mutate(VTax = if_else(
+    class == "U", taxonomy.x, taxonomy.y
+  ))%>%
+  select(MAGs, seq_name,VTax)%>%
+  inner_join(dat_euk_ptax)%>%
+  separate(VTax, into = c("V_Virus","V_Realm","V_Kingdom","V_Phylum","V_Class","V_Order","V_Family", "V_Genus", "V_Species","V_Name"), sep = ";")%>%
+  mutate(goodVirusTax = case_when(
+    V_Class == "Caudoviricetes" & is.na(V_Order) ~ "Caudoviricetes_Unclassified",
+    V_Class == "Caudoviricetes" & V_Order == "unclassifiedCaudoviricetes" ~ "Caudoviricetes_Unclassified",
+    V_Class == "Caudoviricetes" ~ paste("Caudoviricetes", V_Order, sep = "_"),
+    is.na(V_Class) ~ "UnclassifiedClass",
+    .default = V_Class
+  ))%>%
+  select(seq_name, goodVirusTax, MAGs, taxa_good)
+
+dat_virus_euk$goodVirusTax <- gsub(" ", "", dat_virus_euk$goodVirusTax)
+
+dat_virus_sank <- dat_virus_euk %>%
+  make_long(goodVirusTax, taxa_good)
+
+p1 <- ggplot(dat_virus_sank, aes(x = x, 
+               next_x = next_x, 
+               node = node, 
+               next_node = next_node,
+               fill = factor(node), label = node)) +
+  geom_sankey(flow.alpha = .6,
+              node.color = "gray30") +
+  geom_sankey_label(size = 3, color = "white", fill = "gray40") +
+  scale_fill_viridis_d() +
+  theme_sankey(base_size = 18) +
+  labs(x = NULL) +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = .5)) 
+
+p1
+# ggsave_fitmax("Sankey_Genomad_Euk.pdf", p1)
+
+dat_prok_vtax$taxonomy <- gsub(" ", "", dat_prok_vtax$taxonomy)
+dat_prok_vtax$taxonomy <- gsub(";$", "", dat_prok_vtax$taxonomy)
+
+dat_virus_prok <- dat_prok%>%
+  left_join(dat_prok_vtax,by = c("seq_name"),relationship = "many-to-many")%>%
+  mutate(VTax = if_else(
+    class == "U", taxonomy.x, taxonomy.y
+  ))%>%
+  select(MAGs, seq_name,VTax)%>%
+  inner_join(dat_prok_ptax)%>%
+  separate(VTax, into = c("V_Virus","V_Realm","V_Kingdom","V_Phylum","V_Class","V_Order","V_Family", "V_Genus", "V_Species","V_Name"), sep = ";")%>%
+  mutate(goodVirusTax = case_when(
+    V_Class == "Caudoviricetes" & is.na(V_Order) ~ "Caudoviricetes_Unclassified",
+    V_Class == "Caudoviricetes" & V_Order == "unclassifiedCaudoviricetes" ~ "Caudoviricetes_Unclassified",
+    V_Class == "Caudoviricetes" ~ paste("Caudoviricetes", V_Order, sep = "_"),
+    is.na(V_Class) ~ "UnclassifiedClass",
+    .default = V_Class
+  ))%>%
+  mutate(taxa_good = if_else(p == "p__Proteobacteria", c, p))%>%
+  select(seq_name, goodVirusTax, MAGs, taxa_good)
+
+
+
+mostabundantVir <- dat_virus_prok %>%
+  select(-MAGs)%>%
+  distinct()%>%
+  group_by(goodVirusTax)%>%
+  summarise(n = n())%>%
+  arrange(desc(n)) %>%
+  slice(1:15)
+
+# Find the most abundant prokaryotic class
+
+mostabundantProk <- dat_virus_prok %>%
+  select(-MAGs)%>%
+  distinct()%>%
+  group_by(taxa_good)%>%
+  summarise(n = n())%>%
+  arrange(desc(n)) %>%
+  slice(1:15)
+
+# Filter the data to get the most abundant Viral and Prokaryotic classes and remove duplicate viruses that are hosted by bacteria of
+# the same taxonomy
+
+dat_virus_prok_sel <- dat_virus_prok %>%
+  filter(goodVirusTax %in% mostabundantVir$goodVirusTax)%>%
+  filter(taxa_good %in% mostabundantProk$taxa_good)%>%
+  select(-MAGs)%>%
+  distinct()
+
+
+
+dat_virus_sank_p <- dat_virus_prok_sel %>%
+  make_long(goodVirusTax, taxa_good)
+
+p2 <- ggplot(dat_virus_sank_p, aes(x = x, 
+                                 next_x = next_x, 
+                                 node = node, 
+                                 next_node = next_node,
+                                 fill = factor(node), label = node)) +
+  geom_sankey(flow.alpha = .6,
+              node.color = "gray30") +
+  geom_sankey_label(size = 3, color = "white", fill = "gray40") +
+  scale_fill_viridis_d() +
+  theme_sankey(base_size = 18) +
+  labs(x = NULL) +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = .5)) 
+
+p2
+ggsave_fitmax("Sankey_Genomad_Prok.pdf", p1)
+
+
+
